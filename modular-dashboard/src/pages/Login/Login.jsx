31025bba990bfcd5668d7072173aa356a57520ed
jtoledo94 <jtoledo@coloniaexpress.com>
2025-05-06 14:51:35 -0300

    feat: add new classes and handle submit for form

--- modular-dashboard/src/pages/Login/Login.jsx
+++ modular-dashboard/src/pages/Login/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Login.module.css"
 
 const Login = () => {
+
+  const [userInput, setUserInput] = useState(""); 
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(`Usuario:${userInput} Contraseña: ${password}`);
+  }
+
   return (
-    <div className={styles.Login}>Login</div>
+    <div className={styles.login}>
+      <h2>Iniciar Sesión</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input 
+          type="text" 
+          placeholder='Usuario (email o dni)' 
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          required />
+        </div>
+        <div>
+          <input 
+          type="password" 
+          placeholder='Contraseña'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} 
+          required />
+        </div>
+
+        <button type='submit'>Ingresar</button>
+      </form>
+    {errorMessage && <p className='error'>{errorMessage}</p>}
+    </div>
   )
 }
 
